c40823c8f6b2a6327e96acb1db20050f3cf0871f
Maria Teplinskaya <pythagoreantree@googlemail.com>
2022-08-19 23:27:42 +0300

    DP: Min Cost Tickets (fixed)

--- src/main/java/com/maria/dp/DynamicProgramming.java
+++ src/main/java/com/maria/dp/DynamicProgramming.java
@@ -1,48 +1,110 @@
 package com.maria.dp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DynamicProgramming {
 
-    public static int minimum = 6000;
+    public static Map<Integer, Integer> dp = new HashMap<>();
 
     public static int minCostTickets(int[] days, int[] costs) {
-        countCost(days, costs, 0, costs[0], 0);
-        countCost(days, costs, 0, costs[1], 0);
-        countCost(days, costs, 0, costs[2], 0);
-        return minimum;
+        int a = countCost(days, costs, 0, costs[0]);
+        int b = countCost(days, costs, 0, costs[1]);
+        int c = countCost(days, costs, 0, costs[2]);
+        return Math.min(a, Math.min(b, c));
     }
 
-    public static void countCost(int[] days, int[] costs, int index, int cost, int totalCost){
-        int endNum = 0;
-        switch(cost){
-            case 2:
-                endNum = days[index];
-                break;
-            case 7:
-                endNum = days[index] + 6;
-                break;
-            case 15:
-                endNum = days[index] + 29;
-                break;
+    /*public static int countCost1(int[] days, int[] costs, int index, int totalCost) {
+        totalCost += costs[0];
+        int j = index + 1;
+        //if EOF then stop
+        if (j == days.length) {
+            return totalCost;
         }
-        totalCost += cost;
-//        if (totalCost > minimum)
-//            return;
+        if (dp.containsKey(days[j])){
+            return dp.get(days[j]);
+        }
+        //if cost less than minimum now its minimum
+        int one = countCost1(days, costs, j, totalCost);
+        int two = countCost2(days, costs, j, totalCost);
+        int three = countCost3(days, costs, j, totalCost);
+        int minVal = Math.min(one, Math.min(two, three));
+        dp.put(days[index], minVal);
+        return minVal;
+    }
+
+    public static int countCost2(int[] days, int[] costs, int index, int totalCost) {
+        totalCost += costs[1];
+        int j = index;
+        while (j < days.length && days[j] <= (days[index] + 6)){
+            j++;
+        }
+        if (j == days.length) {
+            return totalCost;
+        }
+        if (dp.containsKey(days[j])){
+            return dp.get(days[j]);
+        }
+        //if cost less than minimum now its minimum
+        int one = countCost1(days, costs, j, totalCost);
+        int two = countCost2(days, costs, j, totalCost);
+        int three = countCost3(days, costs, j, totalCost);
+        int minVal = Math.min(one, Math.min(two, three));
+        dp.put(days[index], minVal);
+        return minVal;
+    }
+
+    public static int countCost3(int[] days, int[] costs, int index, int totalCost) {
+        totalCost += costs[2];
+        int j = index;
+        while (j < days.length && days[j] <= (days[index] + 29)){
+            j++;
+        }
+        //if EOF then stop
+        if (j == days.length) {
+            return totalCost;
+        }
+        if (dp.containsKey(days[j])){
+            return dp.get(days[j]);
+        }
+        //if cost less than minimum now its minimum
+        int one = countCost1(days, costs, j, totalCost);
+        int two = countCost2(days, costs, j, totalCost);
+        int three = countCost3(days, costs, j, totalCost);
+        int minVal = Math.min(one, Math.min(two, three));
+        dp.put(days[index], minVal);
+        return minVal;
+    }*/
+
+    public static int countCost(int[] days, int[] costs, int index, int cost){
+        int endNum = 0;
+        if (cost == costs[0])
+            //i need just next index
+            endNum = days[index];
+        else if (cost == costs[1])
+            endNum = days[index] + 6;
+        else if (cost == costs[2])
+            endNum = days[index] + 29;
+        //if (totalCost > minimum)
+        //    return;
         //find first number greater than end number
-        int j = 0;
+        int j = index;
         while (j < days.length && days[j] <= endNum){
             j++;
         }
         //if EOF then stop
         if (j == days.length) {
-            if (totalCost < minimum) {
-                minimum = totalCost;
-            }
-            return;
+            return cost;
+        }
+        if (dp.containsKey(j)){
+            return dp.get(j);
         }
         //if cost less than minimum now its minimum
-        countCost(days, costs, j, costs[0], totalCost);
-        countCost(days, costs, j, costs[1], totalCost);
-        countCost(days, costs, j, costs[2], totalCost);
-
+        int one = cost + countCost(days, costs, j, costs[0]);
+        int two = cost + countCost(days, costs, j, costs[1]);
+        int three = cost + countCost(days, costs, j, costs[2]);
+        int minVal = Math.min(one, Math.min(two, three));
+        dp.put(index, minVal);
+        return minVal;
     }
 }
