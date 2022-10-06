672acbb7d0f8fc9e0d18f4d4bf4f2b77d39835b3
Maria Teplinskaya <pythagoreantree@googlemail.com>
2022-10-07 00:34:45 +0300

    Graph DFS + connected components

--- src/main/java/com/maria/graphs/GraphTraversals.java
+++ src/main/java/com/maria/graphs/GraphTraversals.java
@@ -1,9 +1,64 @@
 package com.maria.graphs;
 
+import java.util.*;
+
+/*
+* For undirected graphs.
+* */
 public class GraphTraversals {
 
-    public static void dfs() {
+    public static Map<Integer, List<Integer>> graph = new HashMap<>();
 
+    static {
+        graph.put(1, List.of(2));
+        graph.put(2, List.of(1, 3, 4));
+        graph.put(3, List.of(1, 2, 4, 5, 6));
+        graph.put(4, List.of(2, 3, 6));
+        graph.put(5, List.of(3));
+        graph.put(6, List.of(3, 4));
+        graph.put(7, new ArrayList<>());
+    }
+
+    /*
+    * DFS for fully connected Graph.
+    * */
+    public static void dfs(Map<Integer, List<Integer>> graph, Integer node, Set<Integer> visited) {
+        if (visited.contains(node)) return;
+        visited.add(node);
+
+        System.out.print(node + " ");
+        List<Integer> neighbors = graph.get(node);
+
+        for (Integer neighbor: neighbors) {
+            dfs(graph, neighbor, visited);
+        }
+    }
+
+    /*
+    * DFS version of not fully connected Graph.
+    * */
+    public static void dfsNFCG(Map<Integer, List<Integer>> graph) {
+        Set<Integer> visited = new HashSet<>();
+        for (Integer key: graph.keySet()) {
+            if (!visited.contains(key)) {
+                dfs(graph, key, visited);
+            }
+        }
+    }
+
+    /*
+    * Count up connected components of a Graph.
+    * */
+    public static Integer countConnectedComponents(Map<Integer, List<Integer>> graph) {
+        Set<Integer> visited = new HashSet<>();
+        int count = 0;
+        for (Integer key: graph.keySet()) {
+            if (!visited.contains(key)) {
+                count++;
+                dfs(graph, key, visited);
+            }
+        }
+        return count;
     }
 
     public static void bfs() {
---
+++ src/main/test/com/maria/graphs/GraphTraversalsTest.java
@@ -0,0 +1,43 @@
+package com.maria.graphs;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+class GraphTraversalsTest {
+
+    public static Map<Integer, List<Integer>> graph = new HashMap<>();
+
+    static {
+        graph.put(1, List.of(2));
+        graph.put(2, List.of(1, 3, 4));
+        graph.put(3, List.of(1, 2, 4, 5, 6));
+        graph.put(4, List.of(2, 3, 6));
+        graph.put(5, List.of(3));
+        graph.put(6, List.of(3, 4));
+        graph.put(7, new ArrayList<>());
+        graph.put(8, new ArrayList<>());
+    }
+
+    @Test
+    void dfs() {
+        GraphTraversals.dfs(graph, 1, new HashSet<>());
+    }
+
+    @Test
+    void dfsNFCG() {
+        GraphTraversals.dfsNFCG(graph);
+    }
+
+    @Test
+    void countConnectedComponents() {
+        int num = GraphTraversals.countConnectedComponents(graph);
+        System.out.println();
+        System.out.println(num);
+    }
+
+    @Test
+    void bfs() {
+    }
+
+}
