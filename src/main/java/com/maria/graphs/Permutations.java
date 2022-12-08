fd327529eee01d757f51ec2db11a443eb5ce944b
Maria Teplinskaya <pythagoreantree@googlemail.com>
2022-12-08 22:18:42 +0300

    Add pairs calculation in the Permutations task

--- src/main/java/com/maria/graphs/Permutations.java
+++ src/main/java/com/maria/graphs/Permutations.java
@@ -1,10 +1,24 @@
 package com.maria.graphs;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Permutations {
 
-    public List<List<Integer>> permute(int[] nums) {
+    public static final Map<Integer, List<Tuple>> pairs = new HashMap<>();
+    public static List<List<Integer>> permute(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            pairs.put(i, new ArrayList<>());
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j) {
+                    int a = nums[i];
+                    int b = nums[j];
+                    pairs.get(i).add(new Tuple(a, b));
+                }
+            }
+        }
         return null;
     }
 }
