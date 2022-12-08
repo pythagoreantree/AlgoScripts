a2a3a5d6d9bebf4f6c120944f49df4e25c008cc5
Maria Teplinskaya <pythagoreantree@googlemail.com>
2022-12-08 23:30:22 +0300

    Add solution for the Permutations task + test

--- src/main/java/com/maria/graphs/Permutations.java
+++ src/main/java/com/maria/graphs/Permutations.java
@@ -4,47 +4,34 @@ import java.util.*;
 
 public class Permutations {
 
-    public static final Map<Integer, List<Tuple>> pairs = new HashMap<>();
+    public static final List<List<Integer>> allChains = new ArrayList<>();
     public static List<List<Integer>> permute(int[] nums) {
-        Set<Integer> allNumbers = new HashSet<Integer>();
-        for(int num: nums) {
-            allNumbers.add(num);
+        allChains.clear();
+
+        Set<Integer> numSet = new HashSet<>();
+        for (int num: nums) {
+            numSet.add(num);
+        }
+        buildChains(new ArrayList<>(), nums, new HashSet<>(numSet));
+        return allChains;
+    }
+
+    private static void buildChains(List<Integer> numList, int[] nums, Set<Integer> numSet) {
+        if (numSet.isEmpty()) {
+            allChains.add(numList);
+            return;
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            pairs.put(i, new ArrayList<>());
-            int a = nums[i];
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j) {
-                    int b = nums[j];
-                    pairs.get(i).add(new Tuple(a, b));
-                }
+        for (int j = 0; j < nums.length; j++) {
+            if (numSet.contains(nums[j])) {
+                List<Integer> currentList = new ArrayList<>(numList);
+                Set<Integer> currentSet = new HashSet<>(numSet);
+                currentList.add(nums[j]);
+                currentSet.remove(nums[j]);
+                buildChains(currentList, nums, currentSet);
             }
         }
 
-        List<List<Integer>> allChains = new ArrayList<>();
-        //для каждой начальной цифры будем строить цепочки
-        for (int k = 0; k < nums.length; k++) {
-            //для каждой цепочки у нас будет свое множество значений
-            Set<Integer> numsK = new HashSet<>(allNumbers);
-            int currentNum = nums[k];
-            numsK.remove(currentNum);
-            List<Integer> chain = new ArrayList<>();
-            chain.add(currentNum);
-            List<List<Integer>> allCurrentChains = new ArrayList<>();
-            while (!numsK.isEmpty()) {
-                List<Tuple> currentNumPairs = pairs.get(currentNum);
-                for(Tuple pair: currentNumPairs) {
-                    if (pair.getX() == currentNum && numsK.contains(pair.getY())) {
-                        List<Integer> l1 = new ArrayList<>(chain);
-                        currentNum = pair.getY();
-                        numsK.remove(currentNum);
-                        l1.add(currentNum);
-                        allCurrentChains.add(l1);
-                    }
-                }
-            }
-        }
-        return null;
+
     }
 }
---
+++ src/main/test/com/maria/graphs/PermutationsTest.java
@@ -0,0 +1,17 @@
+package com.maria.graphs;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PermutationsTest {
+
+    @Test
+    void permute() {
+        int[] nums = new int[]{1, 2, 3};
+        List<List<Integer>> resultLists = Permutations.permute(nums);
+        System.out.println(resultLists);
+    }
+}
