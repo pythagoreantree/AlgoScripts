57ae94de7a82e5c338827af289866835d7f8c8ae
Maria Teplinskaya <pythagoreantree@googlemail.com>
2022-12-08 23:05:58 +0300

    Add bad solution for the Permutations task

--- src/main/java/com/maria/graphs/Permutations.java
+++ src/main/java/com/maria/graphs/Permutations.java
@@ -1,21 +1,47 @@
 package com.maria.graphs;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Permutations {
 
     public static final Map<Integer, List<Tuple>> pairs = new HashMap<>();
     public static List<List<Integer>> permute(int[] nums) {
+        Set<Integer> allNumbers = new HashSet<Integer>();
+        for(int num: nums) {
+            allNumbers.add(num);
+        }
+
         for (int i = 0; i < nums.length; i++) {
             pairs.put(i, new ArrayList<>());
+            int a = nums[i];
             for (int j = 0; j < nums.length; j++) {
                 if (i != j) {
-                    int a = nums[i];
                     int b = nums[j];
                     pairs.get(i).add(new Tuple(a, b));
+                }
+            }
+        }
+
+        List<List<Integer>> allChains = new ArrayList<>();
+        //для каждой начальной цифры будем строить цепочки
+        for (int k = 0; k < nums.length; k++) {
+            //для каждой цепочки у нас будет свое множество значений
+            Set<Integer> numsK = new HashSet<>(allNumbers);
+            int currentNum = nums[k];
+            numsK.remove(currentNum);
+            List<Integer> chain = new ArrayList<>();
+            chain.add(currentNum);
+            List<List<Integer>> allCurrentChains = new ArrayList<>();
+            while (!numsK.isEmpty()) {
+                List<Tuple> currentNumPairs = pairs.get(currentNum);
+                for(Tuple pair: currentNumPairs) {
+                    if (pair.getX() == currentNum && numsK.contains(pair.getY())) {
+                        List<Integer> l1 = new ArrayList<>(chain);
+                        currentNum = pair.getY();
+                        numsK.remove(currentNum);
+                        l1.add(currentNum);
+                        allCurrentChains.add(l1);
+                    }
                 }
             }
         }
